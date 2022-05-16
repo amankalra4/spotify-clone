ada995b01802859192d631fe1204face955c7aaf
Aman Kalra <aman.kalra4@gmail.com>
2022-05-17 00:00:09 +0530

    Added changes for footer

---
+++ src/modules/libs/css-modules.ts
@@ -0,0 +1,7 @@
+export const hoverColor = "#1ed760";
+export const lightGrayColor = "#222326";
+export const legalTermsColor = "#919496";
+
+export const mdScreen = "768px";
+export const lgScreen = "992px";
+export const xlScreen = "992px";
